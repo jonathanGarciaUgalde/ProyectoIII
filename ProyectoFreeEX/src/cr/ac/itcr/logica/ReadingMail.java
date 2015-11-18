82a76db3c271023e51e45d32de7fe8bbb171b438
Dilan Castro Enríquez <greekeshotmail.com>
2015-11-18 13:30:52 -0600

    Prueba Leyendo Email

---
+++ ProyectoFreeEX/src/cr/ac/itcr/logica/ReadingMail.java
@@ -0,0 +1,34 @@
+package cr.ac.itcr.logica;
+
+import java.util.*;
+import javax.mail.*;
+
+
+public class ReadingMail {
+	public void Reader(String email, String password){
+        Properties props = new Properties();
+        props.setProperty("mail.store.protocol", "imaps");
+        try {
+            Session session = Session.getInstance(props, null);
+            Store store = session.getStore();
+            store.connect("imap.gmail.com", email, password);
+            Folder inbox = store.getFolder("INBOX");
+            inbox.open(Folder.READ_ONLY);
+            Message msg = inbox.getMessage(inbox.getMessageCount());
+            Address[] in = msg.getFrom();
+            for (Address address : in) {
+                System.out.println("DE:" + address.toString());
+            }
+            Object mp = (Object) msg.getContent();
+            System.out.println("FECHA:" + msg.getSentDate());
+            System.out.println("ASUNTO:" + msg.getSubject());
+            System.out.println(mp.toString());
+            
+            
+        } catch (Exception mex) {
+            mex.printStackTrace();
+        
+    }
+}
+	
+}
---
+++ ProyectoFreeEX/src/cr/ac/itcr/logica/SendMail.java
@@ -0,0 +1,51 @@
+package cr.ac.itcr.logica;
+
+import java.util.Properties;
+
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+public class SendMail {
+	
+	public static String usuario = "";
+	public static String password = "";
+	String destino;
+    String asunto;
+    String mensaje;
+	private final Properties properties = new Properties();
+	private Session session;
+	
+	
+	
+	public boolean send() {
+		
+		try {
+        	properties.put("mail.smtp.host", "smtp.gmail.com");
+	        properties.put("mail.smtp.starttls.enable", "true");
+	        properties.put("mail.smtp.port", 587);
+	        properties.put("mail.smtp.mail.sender", usuario);
+	        properties.put("mail.smtp.password", password);
+	        properties.put("mail.smtp.user", usuario);
+	        properties.put("mail.smtp.auth", "true");
+	        session = Session.getDefaultInstance(properties);
+	        
+            MimeMessage message = new MimeMessage(session);
+            message.setFrom(new InternetAddress((String) properties.get("mail.smtp.mail.sender")));
+            message.addRecipient(Message.RecipientType.TO, new InternetAddress(destino));
+            message.setSubject(asunto);
+            message.setText(mensaje);
+            Transport t = session.getTransport("smtp");
+            t.connect((String) properties.get("mail.smtp.user"), (String) properties.get("mail.smtp.password"));
+            t.sendMessage(message, message.getAllRecipients());
+            t.close();
+        } 
+        catch (MessagingException send){
+        }
+		return true;
+    }
+
+}
